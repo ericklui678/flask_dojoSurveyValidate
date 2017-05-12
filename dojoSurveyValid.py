57b5da720dfa0d625e8e0d23ea0271625211a7ca
Erick Lui <erickjin.lui@gmail.com>
2017-05-12 13:02:21 -0700

    added validations to check name and comment fields are not blank

--- dojoSurveyValid.py
+++ dojoSurveyValid.py
@@ -8,10 +8,22 @@ def landingPage():
 
 @app.route('/process', methods=['POST'])
 def process():
+    print 'beginning'
     session['name'] = request.form['name']
     session['location'] = request.form['location']
     session['language'] = request.form['language']
     session['comments'] = request.form['comments']
+
+    if len(session['name']) < 1 and len(session['comments']) < 1:
+        flash('Name field is required')
+        flash('Comment field is required')
+        return redirect('/')
+    if len(session['name']) < 1:
+        flash('Name field is required')
+        return redirect('/')
+    if len(session['comments']) < 1:
+        flash('Comment field is required')
+        return redirect('/')
     return redirect('/submit')
 
 @app.route('/submit')
